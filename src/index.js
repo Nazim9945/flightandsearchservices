79be44cf8c754059ca36b4f0c0c1428fbdc57deb
Nazim Saifi <saifinazim2311@gmail.com>
2023-07-11 15:44:52 +0530

    setup basic bodyparser

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 const express=require('express');
+
+const bodyparser=require('body-parser');
+
 const {PORT} =require('./config/serverConfig');
 
 
 const setupAndstartserver=async ()=>{
+
     const app=express();
-    // const PORT=3000;
+    
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({extended:true}));
+
     app.listen(PORT,()=>{
         console.log(`server started at ${PORT}`);
         // console.log(process.env.PORT);
-    })
+    });
+
 }
 
 setupAndstartserver();
